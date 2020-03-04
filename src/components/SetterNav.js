08933979018b2098c0238c1385bc2e5ba91caddf
mooniljoo <ezekielmoon@naver.com>
2020-03-05 00:10:24 +0900

    Update func Counter

--- src/components/SetterNav.js
+++ src/components/SetterNav.js
@@ -1,23 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SetterNav.scss";
 
 function SetterNav() {
+  const [setsCount, setSetsCount] = useState(0);
+
+  const onIncrease = () => {
+    setSetsCount(setsCount + 1);
+  };
+  const onDecrease = () => {
+    setSetsCount(setsCount - 1);
+  };
   return (
     <>
       <div className="SetterNav">
-        <div className="Weight-Setter">
-          <div className="Button-Count-Up">↑</div>
+        <div className="Setter">
+          <div className="Button-Count-Up" onClick={onIncrease}>↑</div>
           <div>
             <input type="text" placeholder="Input your Weight" value="1.25" />
           </div>
-          <div className="Button-Count-Down">↓</div>
+          <div className="Button-Count-Down" onClick={onDecrease}>↓</div>
         </div>
-        <div className="Weight-Setter">
-          <div className="Button-Count-Up">↑</div>
+        <div className="Setter">
+          <div className="Button-Count-Up" onClick={onIncrease}>↑</div>
           <div>
-            <input type="text" placeholder="Input your Set" value="1" />
+            <input type="text" placeholder="Input your Set" value={setsCount<0 ? 0 : setsCount} />
           </div>
-          <div className="Button-Count-Down">↓</div>
+          <div className="Button-Count-Down" onClick={onDecrease}>↓</div>
         </div>
       </div>
     </>
