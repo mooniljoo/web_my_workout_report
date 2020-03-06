38403b9dc330d21f661ff0fdcc77d60cbc3d0bfd
mooniljoo <ezekielmoon@naver.com>
2020-03-06 09:54:30 +0900

    Update WorkoutList.js

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -1,34 +1,34 @@
-import React from "react";
-import Workout from "./Workout";
+import React, { useEffect } from "react";
+// import Workout from "./Workout";
 
-// const Workout = React.memo(function Workout({ workout, onRemove, onToggle }) {
-//   const { title, desc, id, active } = workout;
-//   useEffect(() => {
-//     console.log("workout 값이 설정됨");
-//     console.log(workout);
-//     // return 에 함수를 넣어주면 cleaner 함수 ->  workout가 바뀔때도 사라질때도 그 직전에 호출
-//     return () => {
-//       console.log("workout 값이 바뀌기 전");
-//       console.log(workout);
-//     };
-//   }, [workout]); //deps 값이 바뀔때 마다 호출
-//   return (
-//     <div>
-//       <b
-//         style={{
-//           color: active ? "tan" : "dimgray",
-//           cursor: "pointer"
-//         }}
-//         onClick={() => onToggle(id)}
-//       >
-//         {title}
-//       </b>
-//       &nbsp;
-//       <span>({desc})</span>
-//       <button onClick={() => onRemove(id)}>삭제</button>
-//     </div>
-//   );
-// });
+const Workout = React.memo(function Workout({ workout, onRemove, onToggle }) {
+  const { title, desc, id, active } = workout;
+  useEffect(() => {
+    console.log("workout 값이 설정됨");
+    console.log(workout);
+    // return 에 함수를 넣어주면 cleaner 함수 ->  workout가 바뀔때도 사라질때도 그 직전에 호출
+    return () => {
+      console.log("workout 값이 바뀌기 전");
+      console.log(workout);
+    };
+  }, [workout]); //deps 값이 바뀔때 마다 호출
+  return (
+    <div>
+      <b
+        style={{
+          color: active ? "tan" : "dimgray",
+          cursor: "pointer"
+        }}
+        onClick={() => onToggle(id)}
+      >
+        {title}
+      </b>
+      &nbsp;
+      <span>({desc})</span>
+      <button onClick={() => onRemove(id)}>삭제</button>
+    </div>
+  );
+});
 function WorkoutList({ workouts, onRemove, onToggle }) {
   return (
     <div>
